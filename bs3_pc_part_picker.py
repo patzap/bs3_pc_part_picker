0c8ea6ce165d048ed3b53e5214e189f50612ce4e
patzap <pat.zapata93@gmail.com>
2021-02-28 17:31:27 -0500

    adding title header

--- bs3_pc_part_picker.py
+++ bs3_pc_part_picker.py
@@ -16,24 +16,25 @@ for rows in main_content.find_all('tr'):
     '''
     if (rows.a is not None) and (rows is not main_content.find('div', {'id': 'dg_laptop'})): 
         items = rows.find_all('td')
-        items = [ele.text.strip() for ele in items if ele is not ele.find('td', {'class': 'td__add'})]
+        items = [ele.text.strip() for ele in items if ele is not ele.find('td', {'class': 'td__add'}) and ele != '\n']
         
 
-        #print(items)
+        print(items)
     '''
     data_set = []
 
+    #title_header = rows.find_all('h2')
     if (rows.a is not None) and (rows is not main_content.find('div', {'id': 'dg_laptop'})):
 
+        header = []
         item = rows.find('td', {'class': 'td__item'}).text.strip()
         prev = rows.find('td', {'class': 'td__previous'}).text.strip()
         curr = rows.find('td', {'class': 'td__current'}).text.strip()
-        data_set.append([item, prev, curr])
+        data_set.append([header, item, prev, curr])
 
-    print(data_set)
-        
-
-
+     
+div = main_content.find_all('div')
+print(div)
     
 #outfile.close()
 
